9dce6fba022385a72898afc6669176a711d894ee
amitniz <amitnzri@gmail.com>
2022-11-27 20:30:29 +0200

    update: added svg files support.

--- src/js/components/searchBar.js
+++ src/js/components/searchBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-//import rightArrow from '../../images/right_arrow.svg'
+import right_arrow from '../../images/right_arrow.svg'
 
 
 
@@ -10,17 +10,13 @@ Its main purpose is to recieve a query from the user and parse it.
 props:
 - placeholder: set the placeholder for the search bar
 */
-
-
-
 export default function SearchBar(props){
 
     return(
         <div className = "search-bar-container">
             <input type="text" placeholder={props.placeholder}></input>
             <div className='submit-btn'>
-            <svg xmlns="http://www.w3.org/2000/svg"><path d="m28.15 35.25-1.65-1.6 8.6-8.6H8.6V22.8h26.5l-8.65-8.65 1.65-1.55 11.35 11.3Z"/>
-            </svg>
+            <img className='icon' src={right_arrow}/>
             </div>
         </div>
 
